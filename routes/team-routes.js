f527ce0fc5cd393f167384c829b256bcd3d70032
Jose <alex@MacBook-Pro.local>
2023-01-26 21:46:37 -0500

    finished server setup test

--- routes/team-routes.js
+++ routes/team-routes.js
@@ -6,6 +6,7 @@ const { requireToken } = require('../config/auth')
 
 const router = express.Router()
 
+//index
 router.get('/teams', requireToken, (req, res, next) => {
 	Team.find()
 		.then((team) => {
@@ -14,3 +15,31 @@ router.get('/teams', requireToken, (req, res, next) => {
 		.then((team) => res.status(200).json({ team: team }))
 		.catch(next)
 })
+
+//show
+router.get('/teams/:id', requireToken, (req, res, next) => {
+	Team.findById(req.params.id)
+		.then((team) => res.status(200).json({ team: team }))
+		.catch(next)
+})
+
+//create
+router.post('/teams', requireToken, (req, res, next) => {
+	Team.create(req.body.team)
+		.then((team) => {
+			res.status(201).json({ team: team })
+		})
+		.catch(next)
+})
+
+//update
+router.patch('/teams/:id', requireToken, (req, res, next) => {
+	Team.findById(req.params.id)
+		.then((team) => {
+			return team.updateOne(req.body.team)
+		})
+		.then(() => res.sendStatus(204))
+		.catch(next)
+})
+
+module.exports = router
